5dc22b173bacd2f48c1c26d024c4facc822dcad6
Aswin Suresh Kumar <asure12@illinois.edu>
2025-06-24 18:19:16 +0000

    debugging

--- csv_writer.py
+++ csv_writer.py
@@ -4,14 +4,25 @@ import xml.etree.ElementTree as ET
 
 def ParseXML(csv_filename): 
     output = "output.csv"
-    for file in "output.csv": 
-        tree = ET.parse(file)
-        root = tree.getroot
-        xmlList = []
-        xmlList.append(os.path.abspath(file))
-        #find all method, selected attributes, 
-        #append to list
-        with open(output, 'w') as csvfile: 
-            csvwriter = csv.writer(csvfile)
-            csv.writer(xmlList)
-            
+    with open('locations.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            tree = ET.parse(row[0])
+            print("parsing" + row)
+            root = tree.getroot()
+            xmlList = []
+            print("writing path")
+            xmlList.append(os.path.abspath(row[0]))
+            #find all method, selected attributes, 
+            #append to list
+            with open(output, 'w') as csvfile: 
+                print("writing to file")
+                csvwriter = csv.writer(csvfile)
+                for dirpath, dirnames, filenames in os.walk(row[0]):
+                    for filename in filenames:
+                        if filename.endswith(".xml") and "job" in filenames.lower():
+                            full_path = os.path.join(dirpath, filename)
+                            if os.path.isfile(full_path):  # Only write if it's a file
+                                csvwriter.writerow([full_path])
+                                
+                
--- file_traversal.py
+++ file_traversal.py
@@ -3,18 +3,24 @@ import csv
 
 def findXMLPaths(filepath):  
     if ValidateFilePath(filepath) == True:
+        print("validated")
         WritePathsCSV(filepath)
     else:
         print(f"Path does not exist: {filepath}")
 
 def WritePathsCSV(filepath): 
     xml_file = "locations.csv"
-    for dirpath, dirnames, filenames in os.walk(filepath):
-        for filename in filenames:
-            if filename.endswith(".xml") & filename.lower() == "job":
-                with open(xml_file, 'w') as csvfile:
-                    csvwriter = csv.writer(csvfile)
-                    csvwriter.writerow(os.path.abspath(filename))
+    print("starting os walk")
+    with open(xml_file, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for dirpath, dirnames, filenames in os.walk(filepath):
+            for filename in filenames:
+                if filename.endswith(".xml") and "job" in filename.lower():
+                    #attributes = []
+                    print("found" + filename)
+                    path = os.path.join(dirpath, filename)
+                    #attributes.append(path)
+                    csvwriter.writerow(path)
  
                 
 def ValidateFilePath(filepath):
